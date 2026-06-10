5aee692a72a336e35cb5b8ca2eb04c0ccfb7a178
Nickolas Eagle <eagle.dev.stack@gmail.com>
2026-06-10 11:58:38 +0400

    Включение вызова функции сохранения ключей в основной исполняемый файл

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ import asyncio
 from flags import extract_info_from_flags
 from xui_client import get_clients
 from keys import generate_keys
+from files import save_file_keys
 
 
-def main():
+async def main():
     auth_info = extract_info_from_flags()
-    clients = asyncio.run(get_clients(auth_info))
+    clients = await get_clients(auth_info)
     if not clients:
         return
     keys = generate_keys(clients)
+    success = await save_file_keys(auth_info=auth_info, keys=keys)
+    if not success:
+        return
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
