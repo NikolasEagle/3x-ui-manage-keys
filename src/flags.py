b96716a5920487984da3a5e75444b075687f9c6a
Nickolas Eagle <eagle.dev.stack@gmail.com>
2026-06-10 11:55:25 +0400

    Добавление обработки флага output

--- src/flags.py
+++ src/flags.py
@@ -38,8 +38,19 @@ def extract_info_from_flags() -> AuthInfo:
         help="Inbound ID",
         required=True,
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=str,
+        help="Outut directory to save keys",
+        required=True,
+    )
     args = parser.parse_args()
 
     return AuthInfo(
-        PANEL_URL=args.url, USERNAME=args.username, PASSWORD=args.password, ID=args.id
+        PANEL_URL=args.url,
+        USERNAME=args.username,
+        PASSWORD=args.password,
+        ID=args.id,
+        OUTPUT=args.output,
     )
